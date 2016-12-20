f2f759ce74e6368fc2ba897d74f25a7350e2a0da
Jose Mena <josemena0114@gmail.com>
2016-12-20 17:59:11 +0000

    updating routes and task create form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,19 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  
+  def create
+      @task = Task.new(task_params)
+      
+      if @task.save
+           # #20
+       flash[:notice] = "Task was saved successfully."
+       redirect_to @task
+      else
+       flash.now[:alert] = "Error creating task. Please try again."
+       render :new
+      end
+  end
+  
+  
 end
