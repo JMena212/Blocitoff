c8f5c16b920a8de570f5780488f54c8424121ca5
Jose Mena <josemena0114@gmail.com>
2016-12-23 14:28:52 +0000

    update task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,21 @@
 class TasksController < ApplicationController
   def index
-    @task = current_user.tasks
+    @tasks = current_user.tasks
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new(task_params)
+    if @task.save
+      flash[:notice] = "Task was saved successfully"
+      redirect_to root_path
+    else
+      flash[:alert] = 'Error creating task. Please try again.'
+      render :new
+    end
   end
   
   def create
       @task = Task.new(task_params)
-      
       if @task.save
            # #20
        flash[:notice] = "Task was saved successfully."
@@ -18,6 +24,22 @@ class TasksController < ApplicationController
        flash.now[:alert] = "Error creating task. Please try again."
        render :new
       end
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:notice] = "Task updated"
+    else
+      flash[:alert] = 'Error creating task. Please try again'
+    end
+    redirect_to root_path
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:body, :completed)
   end
   
   
