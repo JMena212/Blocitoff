2c8d3ec6f78710a849a48b05a0916efa16ee2cd7
Jose Mena <josemena0114@gmail.com>
2016-12-24 01:16:11 +0000

    task creation working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,22 +6,15 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = current_user.tasks.new(task_params)
-    if @task.save
-      flash[:notice] = "Task was saved successfully"
-      redirect_to root_path
-    else
-      flash[:alert] = 'Error creating task. Please try again.'
-      render :new
-    end
+    @task = Task.new
   end
   
   def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.new(task_params)
       if @task.save
            # #20
        flash[:notice] = "Task was saved successfully."
-       redirect_to @task
+       redirect_to root_path
       else
        flash.now[:alert] = "Error creating task. Please try again."
        render :new
